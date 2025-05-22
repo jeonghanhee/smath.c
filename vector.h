c26c704c455b73a49cfa04cd53a2476130a9c008
HANHEE JEONG <jeongjiaowo@naver.com>
2025-05-22 13:53:34 +0900

    Update and rename vector.c to vector.h

---
+++ vector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+    double x;
+    double y;
+} vector2;
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} vector3;
+
+#endif
